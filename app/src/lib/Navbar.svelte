c808e538cefc75114bf1ef0cd6a92af19aa30261
jadc <chehimj@gmail.com>
2025-01-30 15:50:56 -0700

    wip tweaks

--- app/src/lib/Navbar.svelte
+++ app/src/lib/Navbar.svelte
@@ -6,33 +6,10 @@
         { name: "Experience", href: "/jobs" },
         { name: "Posts",      href: "/posts" },
     ];
-	let external = [
-        { name: "GitHub",   href: "https://github.com/jadc" },
-        { name: "Linkedin", href: "https://www.linkedin.com/in/jad-c" },
-        { name: "Resume",   href: "/resume" },
-    ];
 </script>
 
-<div {...props}>
-    <header class="flex flex-col md:flex-row p-10 gap-5 bg-slate-900 text-white rounded-b-xl md:rounded-xl">
-        <img class="w-full md:w-1/4 max-w-md m-auto" src="icons/jad.svg" alt="Jad Chehimi">
-        <aside class="flex flex-col gap-2">
-            <div>
-                <h1 class="text-3xl font-semibold">Jad Chehimi</h1>
-                <h2 class="text-xl">Software Developer</h2>
-            </div>
-            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </aside>
-        <nav class="flex flex-row md:flex-col gap-2">
-            {#each external as site (site.href)}
-                <Button label={site.name} href={site.href} icon="github"/>
-            {/each}
-        </nav>
-    </header>
-
-    <nav class="flex w-full gap-2 justify-center my-4">
-        {#each nav as page (page.href)}
-            <Button label={page.name} href={page.href} icon="github"/>
-        {/each}
-    </nav>
-</div>
+<nav {...props}>
+    {#each nav as page (page.href)}
+        <Button label={page.name} href={page.href} icon="github"/>
+    {/each}
+</nav>
--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,13 +1,37 @@
 <script lang="ts">
 	import Navbar from "$lib/Navbar.svelte"
+	import Button from "$lib/Button.svelte"
+
+	let external = [
+        { name: "GitHub",   href: "https://github.com/jadc" },
+        { name: "Linkedin", href: "https://www.linkedin.com/in/jad-c" },
+        { name: "Resume",   href: "/resume" },
+    ];
 
 	import "../app.css";
 	let { children } = $props();
 </script>
 
-<div class="relative max-w-screen-lg m-auto bg-red-400 md:p-4">
-    <Navbar class="fixed w-full mx-auto"/>
-    <main class="p-10 bg-zinc-400">
+<div class="relative max-w-screen-lg mx-auto md:py-4">
+    <header class="flex flex-col md:flex-row p-10 gap-5 bg-slate-900 text-white rounded-b-xl md:rounded-xl">
+        <img class="w-full md:w-1/4 max-w-md m-auto" src="icons/jad.svg" alt="Jad Chehimi">
+        <aside class="flex flex-col gap-2">
+            <div>
+                <h1 class="text-3xl font-semibold">Jad Chehimi</h1>
+                <h2 class="text-xl">Software Developer</h2>
+            </div>
+            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </aside>
+        <nav class="flex flex-row md:flex-col gap-2">
+            {#each external as site (site.href)}
+                <Button label={site.name} href={site.href} icon="github"/>
+            {/each}
+        </nav>
+    </header>
+
+    <Navbar class="sticky top-4 my-4 z-50 max-w-screen-lg inset-x-0 mx-auto flex gap-2 justify-center"/>
+
+    <main class="p-10 mt-20 bg-zinc-400">
         {@render children()}
     </main>
 </div>
