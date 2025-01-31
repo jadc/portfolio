b5b525d13320a8163ae11672bdcd01231e44e989
jadc <chehimj@gmail.com>
2025-01-30 20:58:35 -0700

    navbar

--- app/src/lib/Navbar.svelte
+++ app/src/lib/Navbar.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import Button from "$lib/Button.svelte"
-	let props = $props();
+
+	let { name } = $props();
+
 	let nav = [
         { name: "Projects",   href: "/projects" },
         { name: "Experience", href: "/jobs" },
@@ -8,7 +10,11 @@
     ];
 </script>
 
-<nav {...props}>
+<nav class="sticky top-4 py-2 z-50 max-w-screen-lg inset-x-0 mx-auto flex gap-2 justify-center items-center rounded-full bg-zinc-900 bg-opacity-80 backdrop-blur-sm">
+    <div class="absolute left-10 flex items-center gap-3">
+        <img class="w-10" src="icons/jad.svg" alt="{name}">
+        <h1 class="text-white">{name}</h1>
+    </div>
     {#each nav as page (page.href)}
         <Button label={page.name} href={page.href} icon="github"/>
     {/each}
--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -2,6 +2,8 @@
 	import Navbar from "$lib/Navbar.svelte"
 	import Button from "$lib/Button.svelte"
 
+	const name = "Jad Chehimi";
+
 	let external = [
         { name: "GitHub",   href: "https://github.com/jadc" },
         { name: "Linkedin", href: "https://www.linkedin.com/in/jad-c" },
@@ -14,10 +16,10 @@
 
 <div class="relative max-w-screen-lg mx-auto md:py-4">
     <header class="flex flex-col md:flex-row p-10 gap-5 bg-slate-900 text-white rounded-b-xl md:rounded-xl">
-        <img class="w-full md:w-1/4 max-w-md m-auto" src="icons/jad.svg" alt="Jad Chehimi">
+        <img class="w-full md:w-1/4 max-w-md m-auto" src="icons/jad.svg" alt="{name}">
         <aside class="flex flex-col gap-2">
             <div>
-                <h1 class="text-3xl font-semibold">Jad Chehimi</h1>
+                <h1 class="text-3xl font-semibold">{name}</h1>
                 <h2 class="text-xl">Software Developer</h2>
             </div>
             <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -29,7 +31,7 @@
         </nav>
     </header>
 
-    <Navbar class="sticky top-4 my-4 z-50 max-w-screen-lg inset-x-0 mx-auto flex gap-2 justify-center"/>
+    <Navbar {name}/>
 
     <main class="p-10 mt-20 bg-zinc-400">
         {@render children()}
