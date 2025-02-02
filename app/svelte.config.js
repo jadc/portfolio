d487b2f3a6020d7729c8f5924a3df01c9f2a12f0
jadc <chehimj@gmail.com>
2025-02-02 15:25:38 -0700

    Removed unnecessary path correction

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -8,7 +8,6 @@ const config = {
 	kit: {
 	    // Enable static site generation
         adapter: adapter({ fallback: "404.html" }),
-        paths: { base: process.argv.includes("dev") ? "" : process.env.BASE_PATH }
 	}
 };
 
